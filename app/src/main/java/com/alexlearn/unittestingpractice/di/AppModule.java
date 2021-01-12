222aea1aecb78b7f92c76bdf6576810ccfd1848b
AlexPasynkov <54889913+AlexPasynkov@users.noreply.github.com>
2021-01-12 11:59:49 +0100

    Implementing Room Library

--- app/src/main/java/com/alexlearn/unittestingpractice/di/AppModule.java
+++ app/src/main/java/com/alexlearn/unittestingpractice/di/AppModule.java
@@ -1,9 +1,37 @@
 package com.alexlearn.unittestingpractice.di;
 
 
+import android.app.Application;
+
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.alexlearn.unittestingpractice.persistence.NoteDao;
+import com.alexlearn.unittestingpractice.persistence.NoteDatabase;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
+
+import static com.alexlearn.unittestingpractice.persistence.NoteDatabase.DATABASE_NAME;
 
 @Module
 public class AppModule {
 
+    @Singleton
+    @Provides
+    static NoteDatabase provideNoteDatabase(Application application){
+        return Room.databaseBuilder(
+                application,
+                NoteDatabase.class,
+                DATABASE_NAME
+        ).build();
+    }
+
+    @Singleton
+    @Provides
+    static NoteDao providesNoteDao(NoteDatabase noteDatabase){
+        return noteDatabase.getNoteDao();
+    }
 }
